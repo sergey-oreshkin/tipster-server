926fec37b3b75480400d54f2025b734a064643fc
IrinaSlipchenko <slpchnk.irina@yandex.ru>
2022-11-14 17:41:33 +0300

    spring init

---
+++ src/main/java/com/home/tipster/TipsterApplication.java
@@ -0,0 +1,13 @@
+package com.home.tipster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TipsterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TipsterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/home/tipster/TipsterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.tipster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipsterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
