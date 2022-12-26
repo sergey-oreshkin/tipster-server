59eff579699384a06ae8cc175ddc749193059fba
IrinaSlipchenko <slpchnk.irina@yandex.ru>
2022-12-26 18:37:26 +0300

    feat: add tests service and controller by themes

---
+++ src/test/java/com/home/tipster/themes/service/ThemesMapperTest.java
@@ -0,0 +1,22 @@
+package com.home.tipster.themes.service;
+
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ThemesMapperTest {
+    ThemesMapper mapper = Mappers.getMapper(ThemesMapper.class);
+
+    @Test
+    void toThemes() {
+    }
+
+    @Test
+    void toDto() {
+    }
+
+    @Test
+    void testToDto() {
+    }
+}
